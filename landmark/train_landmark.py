398e78d8a21cfb86bc49fe407705827c89f097d5
athrunsunny <43680123+athrunsunny@users.noreply.github.com>
2022-10-27 19:50:04 +0800

    Update train_landmark.py

--- landmark/train_landmark.py
+++ landmark/train_landmark.py
@@ -401,9 +401,6 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
     if rank in [-1, 0]:
         # Strip optimizers
         final = best if best.exists() else last  # final model
-        for f in [last, best]:
-            if f.exists():
-                strip_optimizer(f)  # strip optimizers
         if opt.bucket:
             os.system(f'gsutil cp {final} gs://{opt.bucket}/weights')  # upload
 
